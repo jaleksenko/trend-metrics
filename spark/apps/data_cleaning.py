19d088d6aa553e3f4b7f7001c5d42aec73a7f507
yaleksenko <aleksenko2405@gmail.com>
2023-11-17 21:34:57 +0100

    ML Branch Completed

--- spark/apps/data_cleaning.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql.functions import col, lower, regexp_replace
-
-def clean_data(df):
-    return df.withColumn("title", lower(col("title"))) \
-        .withColumn("title", regexp_replace(col("title"), "[^a-zA-Z0-9 ]", ""))
--- spark/apps/data_reading.py
+++
@@ -1,9 +0,0 @@
-
-def read_from_kafka(spark, servers, topic):
-    kafka_df = spark.readStream.format("kafka") \
-        .option("kafka.bootstrap.servers", servers) \
-        .option("subscribe", topic) \
-        .option("startingOffsets", "latest") \
-        .load()
-    
-    return kafka_df.selectExpr("CAST(value AS STRING)")
